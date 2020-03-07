27139fb18c152db950c43afbf426861aff081ed8
Nikita Savelev <nsavelev@yahoo.com>
2020-03-07 20:00:59 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,4 +5,4 @@ function lastImageSelector(){return $("div img:last")}
 function ninjaBabySelector(){return $("#baby-ninja")}
 
 function divSelector(){return $(".pics")}
-function firstListItem(){return $("ul #pic-list")}
+function firstListItem(){return $("li ul #pic-list")}
